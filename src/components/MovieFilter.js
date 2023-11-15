8c0df529fd193071d5c142072e19555df1b003e0
gllsrssx <glls@rssx.eu>
2023-11-15 23:43:56 +0100

    Add search functionality to MovieFilter component

--- src/components/MovieFilter.js
+++ src/components/MovieFilter.js
@@ -1,18 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-export function Filter({ films }) {
+export function Filter({ films, onFilter }) {
     const [searchTerm, setSearchTerm] = useState('');
+
+    useEffect(() => {
+        if (searchTerm === '') {
+            onFilter(films);
+        } else {
+            const filteredFilms = films
+                .filter(film => film.name.toLowerCase().includes(searchTerm.toLowerCase()));
+            onFilter(filteredFilms);
+        }
+    }, [searchTerm, films, onFilter]);
 
     const handleSearchTermChange = (event) => {
         setSearchTerm(event.target.value);
     };
 
-    const filteredFilms = films
-        .filter(film => film.name.toLowerCase().includes(searchTerm.toLowerCase()))
-
     return (
-        <div className="filterContainer">
-            <input className="filterSearch" type="text" placeholder="Search..." value={searchTerm} onChange={handleSearchTermChange} />
-        </div>
+        <input className="filterSearch" type="text" placeholder="Search..." value={searchTerm} onChange={handleSearchTermChange} />
     );
 }
