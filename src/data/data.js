f47afa4cc265f71eaba5a5b6e98f1f6fd3d1f806
gllsrssx <glls@rssx.eu>
2023-11-15 23:44:21 +0100

    Update film data and add sample reviews

--- src/data/data.js
+++ src/data/data.js
@@ -1,11 +1,26 @@
 export const FILM_DATA = [
-    {name: "Star Wars", price: 10,genre: "Action", length: 120, image: "https://static.posters.cz/image/1300/posters/star-wars-classic-i97850.jpg"},
-    {name: "Lord Of The Rings", price: 15, genre: "Action", length: 160, image: "https://static.posters.cz/image/750/posters/lord-of-the-rings-fellowship-i11723.jpg"},
-    {name: "Pulp Fiction", price: 11, genre: "Horror", length: 95, image: "https://static.posters.cz/image/750/posters/pulp-fiction-cover-i1288.jpg"},
-    {name: "Star Wars", price: 10,genre: "Action", length: 120, image: "https://static.posters.cz/image/1300/posters/star-wars-classic-i97850.jpg"},
-    {name: "Lord Of The Rings", price: 15, genre: "Action", length: 160, image: "https://static.posters.cz/image/750/posters/lord-of-the-rings-fellowship-i11723.jpg"},
-    {name: "Pulp Fiction", price: 11, genre: "Horror", length: 95, image: "https://static.posters.cz/image/750/posters/pulp-fiction-cover-i1288.jpg"},
-    {name: "Star Wars", price: 10,genre: "Action", length: 120, image: "https://static.posters.cz/image/1300/posters/star-wars-classic-i97850.jpg"},
-    {name: "Lord Of The Rings", price: 15, genre: "Action", length: 160, image: "https://static.posters.cz/image/750/posters/lord-of-the-rings-fellowship-i11723.jpg"},
-    {name: "Pulp Fiction", price: 11, genre: "Horror", length: 95, image: "https://static.posters.cz/image/750/posters/pulp-fiction-cover-i1288.jpg"},
+    {name: "Lord Of The Rings", price: 15, genre: "Fantasy", length: 160, image: "https://static.posters.cz/image/750/posters/lord-of-the-rings-fellowship-i11723.jpg"},
+    {name: "Pulp Fiction", price: 11, genre: "Crime", length: 95, image: "https://static.posters.cz/image/750/posters/pulp-fiction-cover-i1288.jpg"},
+    {name: "The Godfather", price: 12, genre: "Crime", length: 175, image: "https://static.posters.cz/image/750/posters/the-godfather-i6721.jpg"},
+    {name: "The Dark Knight", price: 14, genre: "Action", length: 152, image: "https://static.posters.cz/image/750/posters/the-dark-knight-joker-i7028.jpg"},
+    {name: "Fight Club", price: 10, genre: "Drama", length: 139, image: "https://static.posters.cz/image/750/posters/fight-club-i1287.jpg"},
+    {name: "The Matrix", price: 10, genre: "Sci-Fi", length: 136, image: "https://static.posters.cz/image/750/posters/the-matrix-i7027.jpg"},
 ]
+
+export const sampleReviews = [
+    {
+        name: "John Doe",
+        rating: 4,
+        review: "This webapp has a great selection of movies and is very user-friendly."
+    },
+    {
+        name: "Jane Doe",
+        rating: 5,
+        review: "I love using this webapp to rent movies. The interface is so easy to use!"
+    },
+    {
+        name: "Bob Smith",
+        rating: 3,
+        review: "The movie selection could be better, but overall a decent webapp."
+    }
+];
