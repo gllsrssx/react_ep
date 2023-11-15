56842ccd62851db569a9d41a2d1df59890af787d
gllsrssx <glls@rssx.eu>
2023-11-15 23:44:16 +0100

    Refactor Error component to use functional component

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,21 +1,13 @@
 import React from 'react';
 
-class ErrorPage extends React.Component {
-    goBack = () => {
-        if(this.props.setCurrentComponent) {
-            this.props.setCurrentComponent('home');
-        }
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>Error</h1>
-                <p>Sorry, something went wrong.</p>
-                <button onClick={this.goBack}>Go Back</button>
-            </div>
-        );
-    }
+function Error({ setCurrentComponent }) {
+    return (
+        <div>
+            <h1>Error</h1>
+            <p>Sorry, something went wrong.</p>
+            <button onClick={() => setCurrentComponent('home')}>Go Back</button>
+        </div>
+    );
 }
 
-export default ErrorPage;
+export default Error;
