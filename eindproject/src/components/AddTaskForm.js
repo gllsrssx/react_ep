4ab69f20f993a65d2d5bc5997abe8179c5d71551
gllsrssx <glls@rssx.eu>
2023-11-15 16:40:56 +0100

    Add AddTaskForm component with form to add tasks

---
+++ eindproject/src/components/AddTaskForm.js
@@ -0,0 +1,21 @@
+// AddTaskForm.js
+import React, { useState } from 'react';
+
+function AddTaskForm({ onAdd }) {
+    const [name, setName] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        onAdd(name);
+        setName('');
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" value={name} onChange={(e) => setName(e.target.value)} required />
+            <button type="submit">Voeg taak toe</button>
+        </form>
+    );
+}
+
+export default AddTaskForm;
