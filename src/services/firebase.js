9fbf17d1db1d49c54ac2f0e3103e05d7df168c69
gllsrssx <glls@rssx.eu>
2023-11-15 18:00:40 +0100

    Add Firebase analytics and update configuration.

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,13 +1,22 @@
-import {initializeApp} from "firebase/app";
-import {getFirestore} from 'firebase/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  apiKey: "AIzaSyAPF1KB8Eq9X9cFCIqYOfINNyljAVPqW64",
+  authDomain: "react-ep.firebaseapp.com",
+  projectId: "react-ep",
+  storageBucket: "react-ep.appspot.com",
+  messagingSenderId: "384745751994",
+  appId: "1:384745751994:web:075f257eb78a4c1c950983",
+  measurementId: "G-37369YJVSN"
 };
 
 // Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
-export const firestoreDB = getFirestore(firebaseApp);
-
-console.log("initialized firebase connection");
