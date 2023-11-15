cd1cc543f2ea2b23bef95b4919d6be9c2fdb107a
gllsrssx <glls@rssx.eu>
2023-11-15 23:57:34 +0100

    Update Firebase configuration

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,8 +1,14 @@
 import {initializeApp} from "firebase/app";
 import {getFirestore} from 'firebase/firestore';
 const firebaseConfig = {
-projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-};
+    apiKey: "AIzaSyAPF1KB8Eq9X9cFCIqYOfINNyljAVPqW64",
+    authDomain: "react-ep.firebaseapp.com",
+    projectId: "react-ep",
+    storageBucket: "react-ep.appspot.com",
+    messagingSenderId: "384745751994",
+    appId: "1:384745751994:web:075f257eb78a4c1c950983",
+    measurementId: "G-37369YJVSN"
+  };
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 export const firestoreDB = getFirestore(firebaseApp);
