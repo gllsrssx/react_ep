4efecaf3873266c304c4d7dc085e5a5f8c6b6f13
gllsrssx <glls@rssx.eu>
2023-11-15 16:50:34 +0100

    Refactor App component to use state and add new components

--- eindproject/src/App.js
+++ eindproject/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import TaskList from './TaskList';
+import AddTaskForm from './AddTaskForm';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const handleAdd = (name) => {
+    setTasks([...tasks, { id: Date.now(), name, completed: false }]);
+  };
+
+  const handleToggle = (task) => {
+    setTasks(tasks.map(t => t.id === task.id ? { ...t, completed: !t.completed } : t));
+  };
+
+  const handleDelete = (task) => {
+    setTasks(tasks.filter(t => t.id !== task.id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <AddTaskForm onAdd={handleAdd} />
+      <TaskList tasks={tasks} onToggle={handleToggle} onDelete={handleDelete} />
+      <Footer />
     </div>
   );
 }
