238323d809e7059aa9cf145e989b1c99f5814a91
gllsrssx <glls@rssx.eu>
2023-11-15 23:43:47 +0100

    Add form functionality to ContactUs component

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
 
 export function ContactUs() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(`Name: ${name}, Email: ${email}, Message: ${message}`);
+    };
+
     return (
         <div className="contactUsContainer">
             <h2 className="contactUsTitle">Contact Us</h2>
-            <form className="contactUsForm">
-                <input type="text" placeholder="Your name" />
-                <input type="email" placeholder="Your email" />
-                <textarea placeholder="Your message"></textarea>
-                <button type="submit">Submit</button>
-            </form>
+            <Form className="contactUsForm" onSubmit={handleSubmit}>
+                <Form.Group controlId="formBasicName">
+                    <Form.Control type="text" placeholder="Your name" value={name} onChange={e => setName(e.target.value)} />
+                </Form.Group>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Control type="email" placeholder="Your email" value={email} onChange={e => setEmail(e.target.value)} />
+                </Form.Group>
+                <Form.Group controlId="formBasicMessage">
+                    <Form.Control as="textarea" placeholder="Your message" value={message} onChange={e => setMessage(e.target.value)} />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
         </div>
     );
 }
