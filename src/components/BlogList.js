29ec6de2f4b557a9ec7e1120ac48e74b2e7cfba6
gllsrssx <glls@rssx.eu>
2023-11-15 17:13:08 +0100

    Refactor BlogList component

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,21 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import BlogPost from './BlogPost';
 
-function BlogList() {
-    const [posts, setPosts] = useState([]);
-
-    useEffect(() => {
-        // Fetch posts from Firebase
-        // This is a placeholder and needs to be replaced with actual code
-        const fetchedPosts = []; // Replace with fetched posts
-        setPosts(fetchedPosts);
-    }, []);
-
+function PostList({ posts }) {
     return (
-        <React.Fragment>
-            {posts.map(post => <BlogPost key={post.id} post={post} />)}
-        </React.Fragment>
+        <div>
+            {posts.map((post) => (
+                <BlogPost key={post.id} post={post} />
+            ))}
+        </div>
     );
 }
 
-export default BlogList;
+export default PostList;
