a74be1a88f66b7d9ee7c9d22bbd2a20248c13d6f
gllsrssx <glls@rssx.eu>
2023-11-15 17:26:33 +0100

    Add tabs and components for blog, comments, users, login, and register.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import BlogList from './components/BlogList';
+import AddPostForm from './components/AddPostForm';
+import EditPostForm from './components/EditPostForm';
+import CommentList from './components/CommentList';
+import AddCommentForm from './components/AddCommentForm';
+import UserList from './components/UserList';
+import AddUserForm from './components/AddUserForm';
+import EditUserForm from './components/EditUserForm';
+import Login from './components/Login';
+import Register from './components/Register';
+
+export const UserContext = React.createContext();
 
 function App() {
-  const [tasks, setTasks] = useState([]);
+  const [currentUser, setCurrentUser] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const handleAdd = (name) => {
-    setTasks([...tasks, { id: Date.now(), name, completed: false }]);
-  };
-
-  const handleToggle = (task) => {
-    setTasks(tasks.map(t => t.id === task.id ? { ...t, completed: !t.completed } : t));
-  };
-
-  const handleDelete = (task) => {
-    setTasks(tasks.filter(t => t.id !== task.id));
-  };
+  useEffect(() => {
+    // TODO: Fetch data from Firebase and set the current user and login status
+  }, []);
 
   return (
-    <div>
-      
-    </div>
+    <UserContext.Provider value={{ currentUser, setCurrentUser, isLoggedIn, setIsLoggedIn }}>
+      <Header>
+        <Tabs>
+          <TabList>
+            <Tab>Blog</Tab>
+            <Tab>Comments</Tab>
+            <Tab>Users</Tab>
+            <Tab>Login</Tab>
+            <Tab>Register</Tab>
+          </TabList>
+
+          <TabPanel>
+            <BlogList />
+            {isLoggedIn && <AddPostForm />}
+            {isLoggedIn && <EditPostForm />}
+          </TabPanel>
+          <TabPanel>
+            <CommentList />
+            {isLoggedIn && <AddCommentForm />}
+          </TabPanel>
+          <TabPanel>
+            <UserList />
+            {isLoggedIn && <AddUserForm />}
+            {isLoggedIn && <EditUserForm />}
+          </TabPanel>
+          <TabPanel>
+            <Login />
+          </TabPanel>
+          <TabPanel>
+            <Register />
+          </TabPanel>
+        </Tabs>
+      </Header>
+      <Footer />
+    </UserContext.Provider>
   );
 }
 
