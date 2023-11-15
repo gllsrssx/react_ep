0bf39dc4cc9637dfcc496cb3e1fecf6e7b42b725
gllsrssx <glls@rssx.eu>
2023-11-15 22:12:42 +0100

    Add Homepage component with basic layout

---
+++ src/components/Homepage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {TicketMenu} from "./TicketMenu";
+import {FILM_DATA} from "../data/data";
+import "../App.css"
+
+export function Homepage(props) {
+    return (
+        <div className="homePageContainer">
+            <h1 className="homePageHeader">Welcome to my react library application</h1>
+        </div>
+    );
+}
+
+export default Homepage;
